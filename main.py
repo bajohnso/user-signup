b53feac36c6e72789768b48f6d5f2088d2221bbe
Merry Prankster <blueveinedbeast@gmail.com>
2018-10-03 05:43:41 -0500

    Commit main after re-assigning permissions

--- main.py
+++ main.py
@@ -1,55 +1,79 @@
 from flask import Flask, request, redirect, render_template
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form='''
-<html>
-    <head>
-        <!-- Header -->
-    </head>
+def valid_user_pass(userpass):
+    if userpass == '' or len(userpass) < 3 or len(userpass) > 20:
+        return False   
+    elif ' ' in userpass:
+        return False
+    else: 
+        return True
 
-    <body>
+def valid_confirm(password, confirm):
+    if password == confirm:
+        return True
+    else:
+        return False
 
-        <!-- USERNAME INPUT -->
-        <form action = '' method = 'post'>
-            <label for = 'user'> Username: </label>
-            <input id = 'user' type = 'text' name = 'username' />
-            <input type = 'submit'/>
-        <form> 
-        
-        <br><br>
-        
-        <!-- PASSWORD INPUT -->
-        <form action = '' method = 'post'>
-            <label for = 'password'> Password: </label>
-            <input id = "password" type = 'password' name = 'password'/>
-            <input type = 'submit'/>
-        </form>
+def valid_email(address):
+    if not valid_user_pass(address): 
+        return False
+    if address.count('@') != 1:
+        return False
+    if address.count('.') != 1:
+        return False
+    else:
+        return True
 
-        <br>
+@app.route("/", methods=['GET', 'POST'])
 
-        <!-- VALIDATE PASSWORD -->
-        <form action = '' method = 'post'>
-            <label for = 'validate'> Re-enter password: </label>
-            <input id = 'validate' type = 'password' name = 'validate_password'/>
-            <input type = 'submit'/>
-        </form>
-
-        <br>
-
-        <!-- EMAIL INPUT -->
-        <form action = '' method = 'post'>
-            <label for = 'email'> Email *(optional): </label>
-            <input id = 'email' type = 'text' name = 'email'/>
-            <input type = 'submit'/>
-        </form>
-
-    </body>
-</html>
-'''
-
-@app.route('/', methods=['GET', 'POST'])
 def index():
+#DISPLAY FORM ON LANDING PAGE--
     if request.method == 'GET':
-        return form
+        return render_template('index.html')
+
+    else:
+        u = request.form['username']
+        p = request.form['password']
+        c = request.form['confirm']
+        e = request.form['email']
+
+        u_error = ''
+        p_error = ''
+        c_error = ''
+        e_error = ''
+
+        if not valid_user_pass(u):
+            u_error = 'Username must have 3-20 characters and no spaces.'
+            u = ''
+
+        if not valid_user_pass(p):
+            p_error = 'Password must have 3-20 characters and no spaces.'
+            p = ''
+
+        if not valid_confirm(p,c):
+            c_error = 'Password entries don"t match.'
+            c = ''
+        
+        if not valid_email(e):
+            e_error = 'Email address not valid.'
+            e = ''
+
+    #IF ALL INPUTS VALID, REDIRECT TO WELCOME PAGE
+    if not u_error and not p_error and not c_error and not e_error:
+        return redirect('/welcome')
+    #IF AN INPUT IS INVALID, RETURN FORM WITH ERROR MESSAGES
+    else:
+        return render_template('/')
+
+
+@app.route("/welcome")
+
+def valid_inputs():
+    return 'Welcome!'  
+
+
+app.run()
